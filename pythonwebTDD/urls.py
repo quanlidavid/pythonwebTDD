d14a9ae00d4ee11e34302ee9b08ee76c877c81ba
李泉 <quanlidavid@gmail.com>
2020-01-06 15:00:36 +0800

    First unit test and url mapping, dummy view

--- pythonwebTDD/urls.py
+++ pythonwebTDD/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from lists import views
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^$', views.home_page)
 ]

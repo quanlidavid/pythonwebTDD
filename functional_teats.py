cca532a2e5af288fcb8f671d9a42e600bde4598a
李泉 <quanlidavid@gmail.com>
2020-01-07 11:05:38 +0800

    refactor

--- functional_teats.py
+++ functional_teats.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # open homepage
         self.browser.get("http://localhost:8000")
@@ -34,9 +39,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        # to do item show on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # add another in input box
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -45,14 +49,11 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # the page updated again, and show both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')
         [...]
-
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
